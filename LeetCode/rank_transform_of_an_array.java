59bdcf563d0e6b98ff551f63ae5918c7c239d764
Rohith <98636617+Rohithofficial08@users.noreply.github.com>
2026-07-12 15:08:31 +0530

    solve: #1256 Rank Transform of an Array [Easy]

---
+++ LeetCode/rank_transform_of_an_array.java
@@ -0,0 +1,23 @@
+// Rank Transform of an Array [Easy]
+// https://leetcode.com/problems/rank-transform-of-an-array/
+
+class Solution {
+    public int[] arrayRankTransform(int[] arr) {
+        int[] sorted = arr.clone();
+        Arrays.sort(sorted);
+
+        int m = 0;
+        for (int x : sorted) {
+            if (m == 0 || sorted[m - 1] != x) {
+                sorted[m++] = x;
+            }
+        }
+
+        int[] unique = Arrays.copyOf(sorted, m);
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Arrays.binarySearch(unique, arr[i]) + 1;
+        }
+
+        return arr;
+    }
+}
